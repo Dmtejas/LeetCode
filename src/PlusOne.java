edcd3416263d7e37e8845b2a1d931a05c337658c
Dmtejas <tejastejas0770@gmail.com>
2025-08-23 06:29:43 +0530

    PlusOne

--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,28 +1,42 @@
-class Solution3 {
+class Solution66 {
     public int[] plusOne(int[] digits) {
-        int l = digits.length;
-        if(l == 1 && digits[l-1] == 9) {
-            return new int[]{1, 0};
+        int length = digits.length;
+        if (digits[length - 1] != 9) {
+            digits[length - 1] += 1;
+            return digits;
         }
-        if(digits[l-1] == 9) {
-            digits[l-1] = 0;
-            digits[l-2] += 1;
-        } else if(digits[l-1] == 9 && digits[l-2] == 9) {
-
-        } else {
-            digits[l-1] += 1;
+        int flag = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                flag++;
+            } else {
+                break;
+            }
+        }
+        if (flag == length) {
+            int[] res = new int[length + 1];
+            res[0] = 1;
+            for (int j = 1; j < res.length; j++) {
+                res[j] = 0;
+            }
+            return res;
+        }
+        int pos = length - flag - 1;
+        digits[pos] += 1;
+        for (int j = pos + 1; j < length; j++) {
+            digits[j] = 0;
         }
         return digits;
+
     }
 }
 
-
 public class PlusOne {
     public static void main(String[] args) {
-        int[] digits = {1, 9};
-        Solution3 obj = new Solution3();
-        int[] result = obj.plusOne(digits);
-        for(int n: result) {
+        Solution66 obj = new Solution66();
+        int[] digits = { 4, 9, 9, 9 };
+        int[] res = obj.plusOne(digits);
+        for (int n : res) {
             System.out.println(n);
         }
     }
